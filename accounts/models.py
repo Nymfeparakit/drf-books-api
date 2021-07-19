a30b297ded67767f6d1f7239dc5c026e2b9e6ac3
Nymfeparakit <maxlex047@gmail.com>
2021-07-19 11:25:55 +0300

    Исправлен вторичный ключ

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings
 
 
 class Comment(models.Model):
@@ -7,7 +7,7 @@ class Comment(models.Model):
     Модель комментария пользователя
     """
     owner = models.ForeignKey(
-        'User',
+        settings.AUTH_USER_MODEL,
         on_delete=models.SET_NULL,
         related_name='comments',
         null=True
