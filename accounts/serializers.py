5c0860bcdcec38ed2b87fdec3c82453481633d14
Nymfeparakit <maxlex047@gmail.com>
2021-07-20 21:06:46 +0300

    Исправлено получение комментариев для конкретной книги

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,11 +1,18 @@
 from rest_framework import serializers
 
 from .models import Comment
+from books.models import Book
 
 
 class CommentSerializer(serializers.ModelSerializer):
-    owner = serializers.StringRelatedField()
+    author = serializers.StringRelatedField()
     book = serializers.StringRelatedField()
     class Meta:
         model = Comment
-        fields = ['id', 'owner', 'book', 'created', 'text']
+        fields = ['id', 'author', 'book', 'created', 'text']
+        read_only_fields = ('created',)
+
+    def create(self, validated_data):
+        book = Book.objects.get(pk=self.context["view"].kwargs["book_pk"])
+        validated_data["book"] = book
+        return Comment.objects.create(**validated_data)
