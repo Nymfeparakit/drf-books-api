0b8a0e029cfe65a427eb3d3af35fd70c82bd5a3a
Nymfeparakit <maxlex047@gmail.com>
2021-07-20 21:07:56 +0300

    При добавлении комментариев указывается текущий автор

--- accounts/views.py
+++ accounts/views.py
@@ -1,15 +1,26 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.status import HTTP_201_CREATED
 
 from .models import Comment
 from .serializers import CommentSerializer
-from books.views import IsAuthor
+from books.views import IsAuthorOrReadOnly
 
 
 class CommentViewSet(viewsets.ModelViewSet):
     """
     Viewset для комментариев пользователей
     """
-    queryset = Comment.objects.all()
     serializer_class = CommentSerializer
-    permission_classes = [IsAuthor,]
+    permission_classes = [IsAuthorOrReadOnly,]
+
+    def get_queryset(self):
+        return Comment.objects.filter(book=self.kwargs['book_pk'])
+        
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save(author=request.user)
+            return Response(serializer.data, status=HTTP_201_CREATED)
+
