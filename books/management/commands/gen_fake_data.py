63701cf304c5cc16ddde6dbc8d2d56c51b6b628a
Nymfeparakit <maxlex047@gmail.com>
2021-07-20 23:51:11 +0300

    Добавлено заполнение комментариев, заполнение разных таблиц разнесено по функциям

--- books/management/commands/gen_fake_data.py
+++ books/management/commands/gen_fake_data.py
@@ -7,13 +7,13 @@ import random
 from django.contrib.auth.hashers import make_password
 
 from books.models import Genre, Book, Author
+from accounts.models import Comment
 
 
 genres_names = ['Detective', 'Fantasy', 'Fiction', 'Thriller', 'Horror', 'Sci-Fi', 'Romace', 'Short stories']
+COMMENTS_TEXT = ['Здесь есть комментарий', 'Ну такое', 'Очень даже ничего', 'Не читал, но осуждаю', 'Превосходно']
 # Максимальное количество одновременно вставляемых строк, чтобы не превысить максимальное число вставляемых в sqlite элементов
 MAX_BATCH_SIZE = 500 
-AUTHORS_NUM = 500
-BOOKS_PER_AUTHOR_NUM = 5
 
 class Command(BaseCommand):
     help = "Заполняет базу данных тестовыми данными"
@@ -21,22 +21,14 @@ class Command(BaseCommand):
     def fill_genres(self):
         # генерируем жанры
         genres_objs = [Genre(name=name) for name in genres_names]
-        return Genre.objects.bulk_create(genres_objs)
+        Genre.objects.bulk_create(genres_objs)
+        self.stdout.write("Заполнены жанры")
 
-    def handle(self, *args, **options):
+    def fill_authors(self, fake_date, fake_person):
         ru_provider = RussiaSpecProvider()
-        fake_person = Person('en')
-        fake_date = Datetime()
-
-        # заполняем БД жанрами
-        genres_list = list(self.fill_genres())
-
-        # итерация, после которой будут вставляться книги
-        iter_to_create = AUTHORS_NUM / (AUTHORS_NUM * BOOKS_PER_AUTHOR_NUM / MAX_BATCH_SIZE)
-
         # генерируем авторов
         author_objs = []
-        for i in range(50):
+        for i in range(500):
             author = Author(
                 username=fake_person.email(unique=True),
                 password=make_password('123qwerty123'),
@@ -47,18 +39,48 @@ class Command(BaseCommand):
             )
             author_objs.append(author)
         Author.objects.bulk_create(author_objs)
+        self.stdout.write("Заполнены авторы")
+
+    def fill_books(self, fake_date, fake_person):
+        genres_objs = list(Genre.objects.all())
         books_objs = []
-        for author in Author.objects.all():
+        for i, author in enumerate(Author.objects.all()):
             # Добавляем для автора 5 книг
-            books_objs = []
             for j in range(5):
                 books_objs.append(Book(
-                    genre=random.choice(genres_list),
+                    genre=random.choice(genres_objs),
                     author=author,
                     publ_year=fake_date.year(),
                     title = fake_person.name()
                 ))
-        Book.objects.bulk_create(books_objs)
+                if (i+1)*(j+1) % MAX_BATCH_SIZE:
+                    Book.objects.bulk_create(books_objs)
+                    books_objs = []
+        self.stdout.write("Заполнены книги")
 
+    def fill_comments(self):
+        comments_objs = []
+        authors_list = list(Author.objects.all())
+        for i, book in enumerate(Book.objects.all()):
+            # Добавляем для книги 5 комментариев
+            for j in range(5):
+                comments_objs.append(Comment(
+                    author=random.choice(authors_list),
+                    book=book,
+                    text=random.choice(COMMENTS_TEXT)
+                ))
+                if (i+1)*(j+1) % MAX_BATCH_SIZE:
+                    Comment.objects.bulk_create(comments_objs)
+                    comments_objs = []
+        self.stdout.write("Заполнены комментарии")
+
+    def handle(self, *args, **options):
+        fake_person = Person('en')
+        fake_date = Datetime()
+
+        self.fill_genres()
+        self.fill_authors(fake_date, fake_person)
+        self.fill_books(fake_date, fake_person)
+        self.fill_comments()
 
         self.stdout.write(self.style.SUCCESS("Тестовые данные успешно сгенерированы"))
