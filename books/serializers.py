6137b3a1a691cef47ae386ca0603e59d5bac8402
Nymfeparakit <maxlex047@gmail.com>
2021-07-20 21:10:21 +0300

    Для поля жанр используется вложенный сериализатор

--- books/serializers.py
+++ books/serializers.py
@@ -3,18 +3,19 @@ from rest_framework import serializers
 from .models import Book, Genre, Author
 
 
-class BookSerializer(serializers.ModelSerializer):
-    author = serializers.StringRelatedField()
-    genre = serializers.StringRelatedField()
-    class Meta:
-        model = Book
-        fields = ['id', 'title', 'publ_year', 'author', 'genre']
-
-
 class GenreSerializer(serializers.ModelSerializer):
     class Meta:
         model = Genre
         fields = ['id', 'name']
+
+
+class BookSerializer(serializers.ModelSerializer):
+    author = serializers.StringRelatedField()
+    genre = GenreSerializer()
+    class Meta:
+        model = Book
+        fields = ['id', 'title', 'publ_year', 'author', 'genre']
+        read_only_fields = ('author',)
 
 
 class AuthorSerializer(serializers.ModelSerializer):
