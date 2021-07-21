e0a6c2fadd89338c88226e5ac35c1b7d85702142
Nymfeparakit <maxlex047@gmail.com>
2021-07-21 13:59:56 +0300

    Теперь можно передавать название жанра при создании книги

--- books/serializers.py
+++ books/serializers.py
@@ -11,11 +11,40 @@ class GenreSerializer(serializers.ModelSerializer):
 
 class BookSerializer(serializers.ModelSerializer):
     author = serializers.StringRelatedField()
-    genre = GenreSerializer()
+    genre = serializers.StringRelatedField()
     class Meta:
         model = Book
         fields = ['id', 'title', 'publ_year', 'author', 'genre']
         read_only_fields = ('author',)
+
+    def to_internal_value(self, data):
+        if self.partial and not 'genre' in data:
+            return super().to_internal_value(data)
+        if not 'genre' in data:
+            raise serializers.ValidationError("genre is a required field")
+        genre_name = data.pop('genre')
+        # валидируем все поля, кроме жанра
+        validated_data = super().to_internal_value(data)
+        return dict(validated_data, **{'genre': genre_name})
+
+    def create(self, validated_data):
+        genre_name = validated_data.pop('genre')
+        if not Genre.objects.filter(name=genre_name).exists():
+            raise serializers.ValidationError("Указанный жанр отсутствует")
+        genre = Genre.objects.get(name=genre_name)
+        return Book.objects.create(genre=genre, **validated_data)
+
+    def update(self, instance, validated_data):
+        if self.partial and not 'genre' in validated_data:
+            return super().update(instance, validated_data)
+        genre_name = validated_data.pop('genre')
+        if not Genre.objects.filter(name=genre_name).exists():
+            raise serializers.ValidationError("Указанный жанр отсутствует")
+        instance = super().update(instance, validated_data)
+        instance.genre = Genre.objects.get(name=genre_name)
+        instance.save()
+        return instance
+
 
 
 class AuthorSerializer(serializers.ModelSerializer):
