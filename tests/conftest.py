885e169a309967b5607ac72a1de182a3b2d094c9
Nymfeparakit <maxlex047@gmail.com>
2021-07-21 14:00:41 +0300

    Добавлена фикстура для создания пользователя

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 from rest_framework.test import APIClient
 import pytest
+from mimesis import Person 
 # from pytest_django import fixtures
 
 @pytest.fixture
@@ -7,8 +8,16 @@ def client():
     return APIClient()
 
 @pytest.fixture
+def create_user(django_user_model):
+    fake_person = Person('en')
+    username = fake_person.username()
+    password = '123qwerty123'
+    return django_user_model.objects.create_user(username=username, password=password)
+
+@pytest.fixture
 def authenticated_client(django_user_model):
-    username = 'user1'
+    fake_person = Person('en')
+    username = fake_person.username()
     password = '123qwerty123'
     user = django_user_model.objects.create_user(username=username, password=password)
     client = APIClient()
