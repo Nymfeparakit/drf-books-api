3f3f7ee4092a91ae98723ddad586a3c8613ccd46
Nymfeparakit <maxlex047@gmail.com>
2021-07-21 20:23:47 +0300

    Добавлено ограничение на уникальность для названия жанра

--- books/models.py
+++ books/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import AbstractUser
 
 
 class Genre(models.Model):
-    name = models.CharField(max_length=250)
+    name = models.CharField(max_length=250, unique=True)
 
     def __str__(self):
         return self.name
